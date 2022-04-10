6f1a99910028f57c358b940b50ae3c2957c24ad1
tranduy <tranbaoduy4@gmail.com>
2022-04-10 12:29:34 +0700

    tạo fragment và xử lý navigation

--- app/src/main/java/hcmute/spkt/nguyenphucan19110321/uidesign/MainActivity.java
+++ app/src/main/java/hcmute/spkt/nguyenphucan19110321/uidesign/MainActivity.java
@@ -1,14 +1,54 @@
 package hcmute.spkt.nguyenphucan19110321.uidesign;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import hcmute.spkt.nguyenphucan19110321.uidesign.view.HomeFragment;
+import hcmute.spkt.nguyenphucan19110321.uidesign.view.ListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigation;
+    private NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment;
+            switch (item.getItemId()){
+                case R.id.miHome: {
+                   selectedFragment = new HomeFragment();
+                    break;
+                }
+                case R.id.miList: {
+                    selectedFragment = new ListFragment();
+                    break;
+                }
+                default:
+                    selectedFragment = new HomeFragment();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayoutMain,selectedFragment).commit();
+            return true;
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        setEventNavigation();
+    }
+
+    protected  void setEventNavigation(){
+        bottomNavigation.setOnItemSelectedListener(navListener);
     }
 }
